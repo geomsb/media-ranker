de648daa2f4622ba05c9d21c6305019031ba09b0
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-14 22:43:27 -0700

    methods: self.albums_work, self.books_work, self.movies_work

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,2 +1,15 @@
 class Work < ApplicationRecord
+  validates :category, :title, :creator, :publication_year, :description, presence: true
+
+  def self.albums_work
+    return self.where(category: "album")
+  end
+
+  def self.books_work
+    return self.where(category: "book")
+  end
+
+  def self.movies_work
+    return self.where(category: "movie")
+  end
 end
