9ac4a032aa7427c34e8ddf5ed4fc870911daf511
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-14 21:58:16 -0700

    index view for works

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -3,11 +3,12 @@ class WorksController < ApplicationController
     @works = Work.all
   end
 
+  
+
   private
 	def work_params
-  return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
-End
-
+    return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+  end
 end
 
 
