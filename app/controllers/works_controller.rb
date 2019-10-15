38faa9eee965da3f8956fa5a9511658f79407b55
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-14 21:34:32 -0700

    works routes

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -2,6 +2,12 @@ class WorksController < ApplicationController
   def index
     @works = Work.all
   end
+
+  private
+	def work_params
+  return params.require(:work).permit(:category, :title, :creator, :publication_year, :description)
+End
+
 end
 
 
