3de60f0dca3b8763abf030d17d033e3c67e7f40f
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-14 21:09:36 -0700

    model generated for Work

---
+++ db/migrate/20191015040757_create_works.rb
@@ -0,0 +1,14 @@
+class CreateWorks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :works do |t|
+      t.string :category
+      t.string :title
+      t.string :creator
+      t.integer :publication_year
+      t.string :description
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
