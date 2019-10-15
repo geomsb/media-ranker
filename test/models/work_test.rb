1ac2a1088ef30914e71288e44eaa4e5f2b8edb55
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-15 15:21:25 -0700

    added fixtures

--- test/models/work_test.rb
+++ test/models/work_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 describe Work do
   describe 'validations' do
     before do
-      @work = Work.new(category: "album",	title: "Hello", creator:"Georgina",	publication_year:1989, description: "et expedita non aut quo")
+      @work = Work.new(category: "album",	title: "Ok", creator:"Georgina",	publication_year:1989, description: "et expedita non aut quo")
     end
 
     it 'is valid when all fields are present' do
@@ -15,42 +15,35 @@ describe Work do
       expect(@work.valid?).must_equal false
       expect(@work.errors.messages).must_include :category
     end
-  end
+  
 
-    it "is invalid if another book has the same title" do
-      work1 = Work.new(category: "album",	title: "Hello", creator:"Georgina",	publication_year:1989, description: "et expedita non aut quo")
-      work2 = Work.new(category: "album",	title: "Hello", creator:"Clau",	publication_year:1989, description: "et expedita non aut quo")
-      work1.save
-      work2.save
-
-      expect(work2.valid?).must_equal false
-      expect(work2.errors.messages).must_include :title
+    it 'is invalid if the title is not unique' do
+      @work.title = works(:hello).title
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :title
     end
 
     it 'is invalid without a proper publication year' do
-      work3 = Work.new(category: "album",	title: "Hello", creator:"Clau",	publication_year:1989, description: "et expedita non aut quo")
-      work3.publication_year = "abc"
+      @work.publication_year = "abc"
 
-      expect(work3.valid?).must_equal false
-      expect(work3.errors.messages).must_include :publication_year
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :publication_year
 
-      work3.publication_year = nil
+      @work.publication_year = nil
 
-      expect(work3.valid?).must_equal false
-      expect(work3.errors.messages).must_include :publication_year
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :publication_year
 
-      work3.publication_year = 5
+      @work.publication_year = 5
 
-      expect(work3.valid?).must_equal false
-      expect(work3.errors.messages).must_include :publication_year
+      expect(@work.valid?).must_equal false
+      expect(@work.errors.messages).must_include :publication_year
     end
+  end
 
-  it "retruns a list of all the works related to the album category" do
-    work4 = Work.new(category: "album",	title: "Hello", creator:"Clau",	publication_year:1989, description: "et expedita non aut quo")
-    work5 = Work.new(category: "movies",	title: "Hello", creator:"Clau",	publication_year:1989, description: "et expedita non aut quo")
-    work4.save
-    work5.save
-
-    expect{Work.albums_work}.must_equal 1
+  describe "albums_work" do
+    it "retruns a list of all the works related to the album category" do
+      expect(Work.albums_work.count).must_equal 4
+    end
   end
 end
