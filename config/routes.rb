00bbc84e3c5e44d0f465e0ae3e9176120dbd84c7
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-15 20:13:45 -0700

    user routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   resources :works
+
+  get "/login", to: "users#login_form", as: "login"
+  post "/login", to: "users#login"
+  post "/logout", to: "users#logout", as: "logout"
+  get "/users/current", to: "users#current", as: "current_user"
 end
