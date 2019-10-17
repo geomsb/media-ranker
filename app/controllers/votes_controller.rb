12f7b9f1e8e3dba48af97113d6842f40a52279ea
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-16 22:50:01 -0700

    a user can't vote for the same work twice

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -7,7 +7,7 @@ class VotesController < ApplicationController
   
   def create
     if session[:user_id].nil?
-      flash[:failure] = "A problem occurred: You must log in to do that"
+      flash[:failure] = {general:"A problem occurred: You must log in to do that"}
       redirect_to works_path
     else
       vote_info = {
@@ -15,14 +15,19 @@ class VotesController < ApplicationController
         user_id: session[:user_id],
         work_id: work_id
       }
-      @vote = Vote.new(vote_info) 
-      if @vote.save
-        flash[:success] = "Successfully upvoted" 
+      if Vote.where(user_id: session[:user_id], work_id: work_id).exists?
+        flash[:failure] = {general:"A problem occurred: Could not upvote", user: "user: has already voted for this work"}
         redirect_to works_path
-        return
       else
-        render :new
-        return
+        @vote = Vote.new(vote_info) 
+        if @vote.save
+          flash[:success] = "Successfully upvoted" 
+          redirect_to works_path
+          return
+        else
+          render :new
+          return
+        end
       end
     end
   end
