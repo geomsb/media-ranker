3872a19dbb38a59c0ffa81c6f26448e47a8179f8
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-17 20:37:52 -0700

    votes will have the required fields

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -7,8 +7,15 @@ describe Vote do
     user = users(:georgina)
     @vote = Vote.new(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user.id, work_id: work.id)
   end
+
     it 'is valid when all fields are present' do
       expect(@vote.valid?).must_equal true
+    end
+
+    it "will have the required fields" do
+      [:date, :user_id, :work_id].each do |field|
+        expect(@vote).must_respond_to field
+      end
     end
   end
 end
