1429b0a9d672bd3306b084e1b76801cd9fd95b7e
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-18 13:38:12 -0700

    returns the top ten works for each category

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'pry'
 
 describe User do
   describe 'validations' do
@@ -18,33 +19,36 @@ describe User do
 
   describe 'spotlight' do
     it 'returns the work with more votes' do
-      user = users(:georgina)
-      work = works(:hello)
-      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:21 2019"), user_id: user.id, work_id: work.id)
-      user = users(:claudia)
-      work = works(:hello)
-      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user.id, work_id: work.id)
-      user = users(:claudia)
-      work = works(:world)
-      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:22 2019"), user_id: user.id, work_id: work.id)
+      user1 = users(:georgina)
+      work1 = works(:hello)
+      vote1 = Vote.create(date: Time.parse("Thu Nov 29 14:33:21 2019"), user_id: user1.id, work_id: work1.id)
+      user2 = users(:claudia)
+      work2 = works(:hello)
+      vote2 = Vote.create(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user2.id, work_id: work2.id)
+      user3 = users(:claudia)
+      work3 = works(:world)
+      vote3 = Vote.create(date: Time.parse("Thu Nov 29 14:33:22 2019"), user_id: user3.id, work_id: work3.id)
       
       expect(Vote.spotlight).must_equal works(:hello)
     end
   end 
   
   describe 'top_ten' do
-    it 'returns the top ten works' do
-      user = users(:georgina)
-      work = works(:hello)
-      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:21 2019"), user_id: user.id, work_id: work.id)
-      user = users(:claudia)
-      work = works(:hello)
-      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user.id, work_id: work.id)
-      user = users(:claudia)
-      work = works(:world)
-      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:22 2019"), user_id: user.id, work_id: work.id)
-      
-      expect(Vote.top_ten("album").count).must_equal 
+    it 'returns the top ten works for each category' do
+      user1 = users(:georgina)
+      work1_album = works(:hello)
+      vote1 = Vote.create(date: Time.parse("Thu Nov 29 14:33:21 2019"), user_id: user1.id, work_id: work1_album.id)
+      user2 = users(:claudia)
+      work2_album = works(:hello)
+      vote2 = Vote.create(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user2.id, work_id: work2_album.id)
+      user3 = users(:claudia)
+      work3_album = works(:world)
+      vote3 = Vote.create(date: Time.parse("Thu Nov 29 14:33:22 2019"), user_id: user3.id, work_id: work3_album.id)
+      work4_movies = works(:movies_work)
+      user4 = users(:claudia)
+      vote4 = Vote.create(date: Time.parse("Thu Nov 29 14:33:22 2019"), user_id: user4.id, work_id: work4_movies.id)
+      binding.pry
+      expect(Vote.top_ten("album").count).must_equal 3
     end
   end
 end
