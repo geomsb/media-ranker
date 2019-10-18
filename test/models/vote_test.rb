2402f1f4018d710351da71a364a5d3f8038502e1
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-17 20:34:59 -0700

    votes test validation

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 describe Vote do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+  before do
+    work = works(:hello)
+    user = users(:georgina)
+    @vote = Vote.new(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user.id, work_id: work.id)
+  end
+    it 'is valid when all fields are present' do
+      expect(@vote.valid?).must_equal true
+    end
+  end
 end
