bb4929d05602276fd370aeb33723b8d3c8b059d4
Georgina Sanchez <cpgeorgina.sanchez@gmail.com>
2019-10-18 12:08:45 -0700

    returns the work with more votes

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,4 +15,36 @@ describe User do
       expect(vote.user.id).must_equal user.id
     end
   end
+
+  describe 'spotlight' do
+    it 'returns the work with more votes' do
+      user = users(:georgina)
+      work = works(:hello)
+      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:21 2019"), user_id: user.id, work_id: work.id)
+      user = users(:claudia)
+      work = works(:hello)
+      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user.id, work_id: work.id)
+      user = users(:claudia)
+      work = works(:world)
+      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:22 2019"), user_id: user.id, work_id: work.id)
+      
+      expect(Vote.spotlight).must_equal works(:hello)
+    end
+  end 
+  
+  describe 'top_ten' do
+    it 'returns the top ten works' do
+      user = users(:georgina)
+      work = works(:hello)
+      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:21 2019"), user_id: user.id, work_id: work.id)
+      user = users(:claudia)
+      work = works(:hello)
+      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:20 2019"), user_id: user.id, work_id: work.id)
+      user = users(:claudia)
+      work = works(:world)
+      vote = Vote.create(date: Time.parse("Thu Nov 29 14:33:22 2019"), user_id: user.id, work_id: work.id)
+      
+      expect(Vote.top_ten("album").count).must_equal 
+    end
+  end
 end
